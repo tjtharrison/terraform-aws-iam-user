def8898711237fe0892dfab6191c73c8269ac0ba
tjtharrison <gh@tjth.co>
2022-09-29 19:55:31 +0100

    PR/Added aws_iam_user_login_profile resource and docs for decrypting [minor]

--- example/iam_user.tf
+++ example/iam_user.tf
@@ -2,7 +2,7 @@ module "test_user" {
   source      = "../"
   user_name   = "firstname.surname"
   user_email  = "user@email.com"
-  gpg_pub_key = "[UPDATE_ME]"
+  gpg_pub_key = "UPDATE_ME"
 }
 
 output "password" {
